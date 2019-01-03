6bad136550bd26f7b0a4e822140591187c14b090
min <7kyeongmin@gmail.com>
2019-01-03 10:37:45 +0900

    6 and 7 complete

---
+++ Assets/CustomTrackableEventHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class CustomTrackableEventHandler : DefaultTrackableEventHandler
+{
+    public bool videoContinuous = true;
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+
+        TurnSwitchComponent<TV>(true);
+
+        if (videoContinuous)
+        {
+            var videoPlayers = GetComponentsInChildren<VideoPlayer>(true);
+            foreach (var vp in videoPlayers)
+                vp.Play();
+        }
+        else
+            TurnSwitchComponent<VideoPlayer>(true);
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        TurnSwitchComponent<TV>(false);
+        if (videoContinuous)
+        {
+            var videoPlayers = GetComponentsInChildren<VideoPlayer>(true);
+            foreach (var vp in videoPlayers)
+                vp.Pause();
+        }
+        else
+            TurnSwitchComponent<VideoPlayer>(false);
+    }
+
+    private void TurnSwitchComponent<T>(bool turnOn) where T : Behaviour
+    {
+        var components = GetComponentsInChildren<T>(true);
+        foreach (var c in components)
+            c.enabled = turnOn;
+    }
+}
---
+++ Assets/TV.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TV : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.Rotate(0f, 1f, 0f);
+    }
+}
